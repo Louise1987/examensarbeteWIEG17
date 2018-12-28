fa114920b5d014d5d9dc60c45ff8a068796a3f7f
Louise <louisethomsson@live.se>
2018-12-28 15:27:00 +0100

    Ny fil
    Pga tidigare fel skapat ny CSM

---
+++ EpiserverSiteECommerce/EpiserverSiteECommerce/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace EpiserverSiteECommerce.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
