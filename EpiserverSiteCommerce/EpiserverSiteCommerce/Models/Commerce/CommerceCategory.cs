6c792ea6e02d2b468b687398287e0b4612ad2fcf
Louise <louisethomsson@live.se>
2019-01-11 09:43:02 +0100

    License
    Lisense

---
+++ EpiserverSiteCommerce/EpiserverSiteCommerce/Models/Commerce/CommerceCategory.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.Catalog.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpiserverSiteCommerce.Models.Commerce
+{
+    [CatalogContentType]
+    public class CommerceCategory: NodeContent
+    {
+    }
+}
